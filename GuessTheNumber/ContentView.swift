937abf3e7d160e21eb06957e8e5127b8a3b9da3d
mariya.shabanova <mariya.shabanova@vk.team>
2024-05-01 13:58:42 +0300

    Initial Commit

---
+++ GuessTheNumber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessTheNumber
+//
+//  Created by mariya.shabanova on 01.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by mariya.shabanova on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
